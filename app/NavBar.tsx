8dca64013d58f86443d599775aeaecd91088203e
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-09-16 20:29:42 +0100

    Refactor Navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -11,14 +11,64 @@ import classNames from "classnames";
 import {useSession} from "next-auth/react";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
-import {BsCircleFill} from "react-icons/bs";
 import {FaRegCircleUser} from "react-icons/fa6";
 import {IoBug} from "react-icons/io5";
 
 export default function NavBar() {
-  const path = usePathname();
-  const {status, data} = useSession();
+  return (
+    <nav className="border-b mb-5 px-5 py-3">
+      <Flex justify="between">
+        <Flex align="center" gap="5">
+          <Link href="/">
+            <IoBug size="24" />
+          </Link>
+          <NavLinks />
+        </Flex>
+        <AuthStatus />
+      </Flex>
+    </nav>
+  );
+}
 
+function AuthStatus() {
+  const {data: session, status} = useSession();
+
+  return (
+    <Box>
+      {status === "authenticated" && (
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Avatar
+              src={session.user?.image!}
+              fallback={<FaRegCircleUser />}
+              referrerPolicy="no-referrer"
+              // in case it does not ommit the image error
+              // => see next.config.js
+              radius="full"
+              size="2"
+              className="cursor-pointer"
+            />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Label>
+              <Text size="2">{session.user?.email}</Text>
+            </DropdownMenu.Label>
+            <DropdownMenu.Item>
+              <Link href="/api/auth/signout">Log out</Link>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      )}
+      {status === "unauthenticated" && (
+        <Link className="nav-link" href="/api/auth/signin">
+          Log in
+        </Link>
+      )}
+    </Box>
+  );
+}
+
+function NavLinks() {
   const links = [
     {
       label: "Dashboard",
@@ -29,67 +79,25 @@ export default function NavBar() {
       href: "/issues",
     },
   ];
+  const path = usePathname();
 
   return (
-    <nav className="border-b mb-5 px-5 py-2">
-      <Flex justify="between" align="center">
-        <Flex align="center">
-          <Link href="/" className="my-1 mr-5">
-            <IoBug size="24" />
-          </Link>
-          <ul className="flex space-x-6">
-            {links.map(({label, href}) => {
-              return (
-                <li key={href}>
-                  <Link
-                    className={classNames({
-                      "text-zinc-900": href === path,
-                      "text-zinc-500": href !== path,
-                      "hover:text-zinc-800 transition-all duration-300": 1,
-                    })}
-                    href={href}
-                  >
-                    {label}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </Flex>
-        <Box>
-          {status === "authenticated" && (
-            // <Link href="/api/auth/signout">Log Out</Link>
-            <DropdownMenu.Root>
-              <DropdownMenu.Trigger>
-                <Avatar
-                  src={data.user?.image!}
-                  fallback={<FaRegCircleUser />}
-                  referrerPolicy="no-referrer"
-                  // in case it does not ommit the image error
-                  // => see next.config.js
-                  radius="full"
-                  size="2"
-                  className="cursor-pointer"
-                />
-              </DropdownMenu.Trigger>
-              <DropdownMenu.Content>
-                <DropdownMenu.Label>
-                  <Text size="2">{data.user?.email}</Text>
-                </DropdownMenu.Label>
-                <DropdownMenu.Item>
-                  <Link href="/api/auth/signout">Log out</Link>
-                </DropdownMenu.Item>
-              </DropdownMenu.Content>
-            </DropdownMenu.Root>
-          )}
-          {status === "unauthenticated" && (
-            <Link href="/api/auth/signin">Log in</Link>
-          )}
-          {status === "loading" && (
-            <BsCircleFill size="32" color="lightgray" />
-          )}
-        </Box>
-      </Flex>
-    </nav>
+    <ul className="flex space-x-6">
+      {links.map(({label, href}) => {
+        return (
+          <li key={href}>
+            <Link
+              className={classNames({
+                "nav-link": true,
+                "!text-zinc-900": href === path,
+              })}
+              href={href}
+            >
+              {label}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
