04826766b5397d0f796796dad5682994f7b62376
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-09-18 13:33:54 +0100

    Build the layout of pagination component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,6 +2,7 @@ import {IssueActionBtn} from "@/app/components";
 import {Issue, Status} from "@prisma/client";
 import {Suspense} from "react";
 import {getIssues} from "../_lib/data-service";
+import Pagination from "../components/Pagination";
 import IssueList from "./IssueList";
 import IssueListFallback from "./issueListFallback";
 import IssueStatusFilter from "./IssueStatusFilter";
@@ -15,16 +16,16 @@ export const statusOptions: Status[] = [
 export const revalidate = 0;
 
 export default async function Page({
-  searchParams: {status, sortBy},
+  searchParams: {status, page = 1},
 }: {
-  searchParams: {status?: Status; sortBy?: keyof Issue};
+  searchParams: {status?: Status; page: number};
 }) {
   const s = status
     ? statusOptions.includes(status)
       ? status
       : "All"
     : "All";
-  const issues: Issue[] | string = await getIssues(s, sortBy);
+  const issues: Issue[] | string = await getIssues(s);
   if (typeof issues === "string") {
     throw new Error(issues);
   }
@@ -38,6 +39,11 @@ export default async function Page({
       <Suspense fallback={<IssueListFallback />}>
         <IssueList issues={issues} />
       </Suspense>
+      <Pagination
+        itemCount={issues.length}
+        pageSize={6}
+        currentPage={page}
+      />
     </div>
   );
 }
